041b15f6003d0bdf6883cece01842e9486319ebc
Will E Sonis <133854827+wesonis@users.noreply.github.com>
2026-03-12 04:15:06 -0400

    Update Make_Linux.sh for improved compilation
    Refactor Make_Linux.sh to use variables for architecture and flags.

--- V_02_2015/Make_Linux.sh
+++ V_02_2015/Make_Linux.sh
@@ -1,16 +1,26 @@
-nvcc -O3 -gencode arch=compute_30,code=sm_30 -odir "src" -M -o "src/main.d" "src/main.cpp"
-nvcc -O3 --compile  -x c++ -o  "src/main.o" "src/main.cpp"
+#!/bin/bash
 
-nvcc -O3 -gencode arch=compute_30,code=sm_30 -odir "src/Interface" -M -o "src/Interface/KDevice.d" "src/Interface/KDevice.cpp"
-nvcc -O3 --compile  -x c++ -o  "src/Interface/KDevice.o" "src/Interface/KDevice.cpp"
-nvcc -O3 -gencode arch=compute_30,code=sm_30 -odir "src/Interface" -M -o "src/Interface/KHost.d" "src/Interface/KHost.cpp"
+ARCH="-gencode arch=compute_75,code=sm_75"
+STD="-std=c++14"
+CXXFLAGS="-O3 $STD"
+NVFLAGS="-O3 $STD $ARCH --allow-unsupported-compiler -ccbin g++-10"
 
-nvcc -O3 --compile  -x c++ -o  "src/Interface/KHost.o" "src/Interface/KHost.cpp"
-nvcc -O3 -gencode arch=compute_30,code=sm_30 -odir "src/Input" -M -o "src/Input/KSimulationData.d" "src/Input/KSimulationData.cpp"
+mkdir -p Release
 
-nvcc -O3 --compile  -x c++ -o  "src/Input/KSimulationData.o" "src/Input/KSimulationData.cpp"
-nvcc -O3 -gencode arch=compute_30,code=sm_30 -odir "src/Device" -M -o "src/Device/DeviceInterface.d" "src/Device/DeviceInterface.cu"
+# host compilation
+g++ $CXXFLAGS -c src/main.cpp -o src/main.o
+g++ $CXXFLAGS -c src/Interface/KDevice.cpp -o src/Interface/KDevice.o
+g++ $CXXFLAGS -c src/Interface/KHost.cpp -o src/Interface/KHost.o
+g++ $CXXFLAGS -c src/Input/KSimulationData.cpp -o src/Input/KSimulationData.o
 
-nvcc --compile -O3 -gencode arch=compute_30,code=compute_30 -gencode arch=compute_30,code=sm_30  -x cu -o  "src/Device/DeviceInterface.o" "src/Device/DeviceInterface.cu" 
+# device compilation (with older GCC)
+nvcc $NVFLAGS -c src/Device/DeviceInterface.cu -o src/Device/DeviceInterface.o
 
-nvcc --cudart static -link -o  "Release/V_02_2015"  src/main.o  src/Interface/KDevice.o src/Interface/KHost.o  src/Input/KSimulationData.o  src/Device/DeviceInterface.o   -lGL -lGLU -lglut
+# linking
+nvcc --cudart static -o Release/V_02_2015 \
+    src/main.o \
+    src/Interface/KDevice.o \
+    src/Interface/KHost.o \
+    src/Input/KSimulationData.o \
+    src/Device/DeviceInterface.o \
+    -lGL -lGLU -lglut
